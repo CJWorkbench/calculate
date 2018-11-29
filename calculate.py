339cccff7dd18c7881b33efa874aa6b18263224e
jstray <jonathanstray@gmail.com>
2018-11-29 18:06:14 -0500

    New output column names, better tests

--- calculate.py
+++ calculate.py
@@ -14,22 +14,33 @@ op_functions = {
     'X is what percent of Y' : lambda x,y: x/y
     }
 
-op_names = {
-    'Add' : 'Sum',
-    'Subtract' : 'result',
-    'Multiply' : 'Product',
-    'Divide' : 'result',
-    'Average' : 'Average',
-    'Median' : 'Median',
-    'Minimum' : 'Minimum',
-    "Maximum" : 'Minimum',
-    'Percentage change' : 'percentage_change',
-    'X percent of Y' : 'result',
-    'X is what percent of Y' : 'percentage'
+op_result_names = {
+    'Add' : 'Sum of',
+    'Subtract' : '{col1} minus {col2}',
+    'Multiply' : 'Product of',
+    'Divide' : '{col1} divided by {col2}',
+    'Average' : 'Average of',
+    'Median' : 'Median of',
+    'Minimum' : 'Minimum of',
+    "Maximum" : 'Minimum of',
+    'Percentage change' : 'Percent change {col1} to {col2}',
+    'X percent of Y' : '{col1} percent of {col2}',
+    'X is what percent of Y' : '{col1} is this percent of {col2}'
     }
 
 # Operations which take an arbitrary number of columns
 multicolumn_ops = ['Add','Multiply','Average','Median','Minimum','Maximum']
+
+# Formatters to produce result column names
+def format_two_cols(fstring, col1, col2):
+    return fstring.format(col1=col1, col2=col2)
+
+def format_multicols(prefix_string, cols):
+    if len(cols) < 4:
+        return prefix_string + ' ' + ', '.join(cols)
+    else:
+        return prefix_string + ' {num} columns'.format(num=len(cols))
+
 
 def render(table, params):
     operation_strings = 'Add|Subtract|Multiply|Divide||Average|Median|Minimum|Maximum||Percentage change|X percent of Y|X is what percent of Y'.split('|')
@@ -45,12 +56,15 @@ def render(table, params):
         if colnames == '':
             return table  # waiting for paramter, do nothing
         colnames = colnames.split(',')
+        if len(colnames) < 2:
+            return table  # need at least two columns to operate
 
         for name in colnames:
             if not is_numeric_dtype(table[name]):
                 return "Column " + name + " is not numbers"
 
-        table[op_names[operation]]=(table[colnames]).agg(op_functions[operation], axis=1)
+        newcolname = format_multicols(op_result_names[operation], colnames)
+        table[newcolname]=(table[colnames]).agg(op_functions[operation], axis=1)
 
     else:
         # two column operations (subtract, percentage, ...)
@@ -67,7 +81,8 @@ def render(table, params):
         if not is_numeric_dtype(table[col2]):
             return "Column " + col2 + " is not numbers"
 
-        table[op_names[operation]] = op_functions[operation](table[col1], table[col2])
+        newcolname = format_two_cols(op_result_names[operation], col1, col2)
+        table[newcolname] = op_functions[operation](table[col1], table[col2])
         
     return table
 
--- test_calculate.py
+++ test_calculate.py
@@ -2,26 +2,24 @@ import unittest
 import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from calculate import render, op_names, op_functions, multicolumn_ops
+from calculate import render
 
 # turn menu strings into indices for parameter dictionary
 # must be kept in sync with filter.json
 menutext = "Add|Subtract|Multiply|Divide||Average|Median|Minimum|Maximum||Percentage change|X percent of Y|X is what percent of Y"
 menu = menutext.split('|')
 
-two_column_ops = set(menu) - set(multicolumn_ops) - set(['']) # remove empty item from ||
-
 class TestCalculate(unittest.TestCase):
 
     def setUp(self):
         # Test data includes some non number columns and some nulls and values to check type handling
         self.table = pd.DataFrame(
-            [['fred', 2, 3, 4.5],
-             ['frederson', 5, 6, 7.5],
-             [np.nan, np.nan, np.nan, np.nan],
-             ['maggie', 8, 9, 10.5],
-             ['Fredrick', 11, 12, 13]],
-            columns=['a', 'b', 'c', 'd'])
+            [['fred', 2, 3, 4.5, 1],
+             ['frederson', 5, 6, 7.5, 1],
+             [np.nan, np.nan, np.nan, np.nan, np.nan],
+             ['maggie', 8, 9, 10.5, 1],
+             ['Fredrick', 11, 12, 13, 1]],
+            columns=['a', 'b', 'c', 'd', 'e'])
 
     def test_no_multicolumn(self):
         # Missing columns on a multi-column operation
@@ -41,27 +39,60 @@ class TestCalculate(unittest.TestCase):
         out = render(self.table, params)
         self.assertTrue(out.equals(self.table))  # should NOP when first applied
 
-    def test_multicolumn_ops(self):
-        for op in multicolumn_ops:
-            params = {'operation':menu.index(op), 'colnames':'b,c,d', 'col1':'', 'col2':''}
-            out = render(self.table, params)
-            newtab = self.table.copy()
-            newtab[op_names[op]] = newtab[['b','c','d']].agg(op_functions[op], axis=1)
-            self.assertTrue(out.equals(newtab)) 
+    def test_multicolumn_add(self):
+        params = {'operation':menu.index('Add'), 'colnames':'b,c,d', 'col1':'', 'col2':''}
+        newtab = self.table.copy()
+        newtab['Sum of b, c, d'] = newtab[['b','c','d']].agg('sum', axis=1)
+        out = render(self.table, params)
+        self.assertTrue(out.equals(newtab)) 
+
+    def test_multicolumn_add_many_cols(self):
+        # test with four columns to check column naming case
+        params = {'operation':menu.index('Add'), 'colnames':'b,c,d,e', 'col1':'', 'col2':''}
+        newtab = self.table.copy()
+        newtab['Sum of 4 columns'] = newtab[['b','c','d','e']].agg('sum', axis=1)
+        out = render(self.table, params)
+        self.assertTrue(out.equals(newtab)) 
+
+    def test_multicolumn_nop(self):
+        # if only one column supplied, does nothing
+        params = {'operation':menu.index('Multiply'), 'colnames':'c', 'col1':'', 'col2':''}
+        newtab = self.table.copy()
+        out = render(self.table, params)
+        self.assertTrue(out.equals(newtab)) 
+
+    def test_multicolumn_average(self):
+        params = {'operation':menu.index('Average'), 'colnames':'b,c,d', 'col1':'', 'col2':''}
+        newtab = self.table.copy()
+        newtab['Average of b, c, d'] = newtab[['b','c','d']].agg('mean', axis=1)
+        out = render(self.table, params)
+        self.assertTrue(out.equals(newtab)) 
 
     def test_multicolumn_non_numeric(self):
         params = {'operation':menu.index("Add"), 'colnames':'a,b', 'col1':'', 'col2':''}
         out = render(self.table, params)
-        self.assertTrue(isinstance(out, str))         
+        self.assertTrue(isinstance(out, str))      
 
-    def test_two_column_ops(self):
-        print(two_column_ops)
-        for op in two_column_ops:
-            params = {'operation':menu.index(op), 'colnames':'', 'col1':'c', 'col2':'d'}
-            out = render(self.table, params)
-            newtab = self.table.copy()
-            newtab[op_names[op]] = op_functions[op](newtab['c'], newtab['d'])
-            self.assertTrue(out.equals(newtab)) 
+    def test_two_column_subtract(self):
+        params = {'operation':menu.index('Subtract'), 'colnames':'', 'col1':'c', 'col2':'d'}
+        out = render(self.table, params)
+        newtab = self.table.copy()
+        newtab['c minus d'] = newtab['c'] - newtab['d']
+        self.assertTrue(out.equals(newtab)) 
+
+    def test_two_column_percent_change(self):
+        params = {'operation':menu.index('Percentage change'), 'colnames':'', 'col1':'e', 'col2':'b'}
+        newtab = self.table.copy()
+        newtab['Percent change e to b'] = (newtab['b'] - newtab['e']) / newtab['e']
+        out = render(self.table, params)
+        self.assertTrue(out.equals(newtab)) 
+
+    def test_two_column_percentage_of(self):
+        params = {'operation':menu.index('X percent of Y'), 'colnames':'', 'col1':'e', 'col2':'b'}
+        newtab = self.table.copy()
+        newtab['e percent of b'] = newtab['b'] * newtab['e']
+        out = render(self.table, params)
+        self.assertTrue(out.equals(newtab)) 
 
 if __name__ == '__main__':
     unittest.main()
