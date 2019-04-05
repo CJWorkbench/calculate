d6e7c5284fa75125bf9ace5ce12b8ffd7d250895
Adam Hooper <adam@adamhooper.com>
2019-04-05 15:48:37 -0400

    Refactor: use classes.
    Rather than spread operations across 7 global variables, just use two
    classes and one global variable.

--- calculate.py
+++ calculate.py
@@ -1,49 +1,56 @@
+from dataclasses import dataclass
+from typing import Callable, List
+import pandas as pd
 from pandas.api.types import is_numeric_dtype
 
-op_functions = {
-    'add': 'sum',
-    'subtract': lambda x, y: x - y,
-    'multiply': 'product',
-    'divide': lambda x, y: x / y,
-    'mean': 'mean',
-    'median': 'median',
-    'minimum': 'min',
-    'maximum': 'max',
-    'percent_change': lambda x, y: (y - x) / x,
-    'percent_multiply': lambda x, y: x * y,
-    'percent_divide': lambda x, y: x / y,
+
+@dataclass
+class MulticolumnOp:
+    agg: str
+    """Argument to pass to pd.DataFrame.agg()."""
+
+    default_result_column_format: str
+    """op.default_result_column_format.format('x, y') => 'Sum of x, y'."""
+
+    def default_result_column_name(self, colnames: List[str]) -> str:
+        """op.default_result_column_name(['x', 'y']) => 'Sum of x, y'."""
+        if len(colnames) < 4:
+            colnames_str = ', '.join(colnames)
+        else:
+            colnames_str = f'{len(colnames)} columns'
+
+        return self.default_result_column_format.format(cols=colnames_str)
+
+
+@dataclass
+class BinaryOp:
+    fn: Callable[[pd.Series, pd.Series], pd.Series]
+    """Function to operate on two Series, returning a Series."""
+
+    default_result_column_format: str
+    """op.default_result_column_format.format('x', 'y') => 'x minus y'."""
+
+    def default_result_column_name(self, col1: str, col2: str) -> str:
+        """op.default_result_column_name('x', 'y') => 'Sum of x, y'."""
+        return self.default_result_column_format.format(col1=col1, col2=col2)
+
+
+Operations = {
+    'add': MulticolumnOp('sum', 'Sum of {cols}'),
+    'subtract': BinaryOp(lambda x, y: x - y, '{col1} minus {col2}'),
+    'multiply': MulticolumnOp('product', 'Product of {cols}'),
+    'divide': BinaryOp(lambda x, y: x / y, '{col1} divided by {col2}'),
+    'mean': MulticolumnOp('mean', 'Average of {cols}'),
+    'median': MulticolumnOp('median', 'Median of {cols}'),
+    'minimum': MulticolumnOp('min', 'Minimum of {cols}'),
+    'maximum': MulticolumnOp('max', 'Maximum of {cols}'),
+    'percent_change': BinaryOp(lambda x, y: (y - x) / x,
+                               'Percent change {col1} to {col2}'),
+    'percent_multiply': BinaryOp(lambda x, y: x * y,
+                                 '{col1} percent of {col2}'),
+    'percent_divide': BinaryOp(lambda x, y: x / y,
+                               '{col1} is this percent of {col2}'),
 }
-
-op_result_names = {
-    'add': 'Sum of {cols}',
-    'subtract': '{col1} minus {col2}',
-    'multiply': 'Product of {cols}',
-    'divide': '{col1} divided by {col2}',
-    'mean': 'Average of {cols}',
-    'median': 'Median of {cols}',
-    'minimum': 'Minimum of {cols}',
-    'maximum': 'Maximum of {cols}',
-    'percent_change': 'Percent change {col1} to {col2}',
-    'percent_multiply': '{col1} percent of {col2}',
-    'percent_divide': '{col1} is this percent of {col2}',
-}
-
-# Operations which take an arbitrary number of columns
-multicolumn_ops = {'add', 'multiply', 'mean', 'median', 'minimum', 'maximum'}
-
-
-# Formatters to produce result column names
-def format_two_cols(fstring, col1, col2):
-    return fstring.format(col1=col1, col2=col2)
-
-
-def format_multicols(fstring, cols):
-    if len(cols) < 4:
-        cols_str = ', '.join(cols)
-    else:
-        cols_str = f'{len(cols)} columns'
-
-    return fstring.format(cols=cols_str)
 
 
 def get_single_value(table, params):
@@ -64,16 +71,16 @@ def get_single_value(table, params):
 
 
 def render(table, params):
-    operation = params['operation']
-
-    if not operation:
+    if not params['operation']:
         return table  # waiting for paramter, do nothing
 
-    if operation in multicolumn_ops:
+    operation = Operations[params['operation']]
+
+    if isinstance(operation, MulticolumnOp):
         # multiple column operations (add, average...)
 
         extra_scalar = (
-            (operation == 'add' or operation == 'multiply')
+            operation.agg in {'sum', 'product'}
             and params['single_value_selector'] != 'none'
         )
 
@@ -93,16 +100,15 @@ def render(table, params):
         if params['outcolname']:
             newcolname = params['outcolname']
         else:
-            newcolname = format_multicols(op_result_names[operation], colnames)
-        table[newcolname] = table[colnames].agg(op_functions[operation],
-                                                axis=1)
+            newcolname = operation.default_result_column_name(colnames)
+        table[newcolname] = table[colnames].agg(operation.agg, axis=1)
 
         # Optional add/multiply all rows by a scalar
         if extra_scalar:
             val = get_single_value(table, params)
             if isinstance(val, str):
                 return val  # error essage
-            if operation == 'add':
+            if operation.agg == 'sum':
                 table[newcolname] += val
             else:
                 table[newcolname] *= val
@@ -124,8 +130,6 @@ def render(table, params):
         if params['outcolname']:
             newcolname = params['outcolname']
         else:
-            newcolname = format_two_cols(op_result_names[operation],
-                                         col1, col2)
-        table[newcolname] = op_functions[operation](table[col1],
-                                                    table[col2])
+            newcolname = operation.default_result_column_name(col1, col2)
+        table[newcolname] = operation.fn(table[col1], table[col2])
     return table
